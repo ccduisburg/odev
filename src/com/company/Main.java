18693ef3c63c5f740c528fcafc7b16aca5eda372
cc <cemilkabakci@gmail.com>
2020-04-11 22:53:20 +0200

    odev1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ddd");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter an first integer: ");
+        int deger1 = keyboard.nextInt();
+        System.out.println("enter an second integer: ");
+        int deger2 = keyboard.nextInt();
+        System.out.println(sonuc(deger1,deger2));
+
+    }
+
+    private static String sonuc(Integer deger1, Integer deger2){
+        if (deger1>deger2){
+            return "ilk deger büyük";
+        } else {
+            return "ikinci deger büyük";
+        }
+
     }
 }
