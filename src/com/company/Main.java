e887e34338c23a7abef00125e608e633c21f3736
ccduisburg <cemilkabakci@gmail.com>
2020-04-12 13:12:50 +0200

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("enter an first integer: ");
-        int deger1 = keyboard.nextInt();
+        int sayi1 = keyboard.nextInt();
         System.out.println("enter an second integer: ");
-        int deger2 = keyboard.nextInt();
-        System.out.println(sonuc(deger1,deger2));
+        int sayi2 = keyboard.nextInt();
+     //   System.out.println(sonuc(deger1,deger2));
+       String sonuc =karsilastirma(sayi1,sayi2);
+        System.out.println(sonuc);
 
     }
 
-    private static String sonuc(Integer deger1, Integer deger2){
+    private static String karsilastirma (Integer deger1, Integer deger2){
         if (deger1>deger2){
             return "ilk deger büyük";
-        } else {
+        }  else if(deger1==deger2){
+            return "iki deger esittir";
+        }else{
             return "ikinci deger büyük";
         }
 
